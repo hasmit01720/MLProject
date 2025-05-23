f9192fac6b9f25b878922e76065baee9f2caa951
Hasmit Patel <hasmit01720@gmail.com>
2025-05-23 22:27:58 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,7 +7,6 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
-
 from src.exception import CustomerException
 from src.logger import logging
 import os
@@ -71,7 +70,7 @@ class DataTransformation:
             return preprocessor
         
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomerException(e,sys)
         
     def initiate_data_transformation(self,train_path,test_path):
 
@@ -121,4 +120,4 @@ class DataTransformation:
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomerException(e,sys)
